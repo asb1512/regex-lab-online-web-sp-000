0152ad2c29af0e3f558bb379f093017a62b91b44
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-30 20:05:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,7 @@ end
 # Returns true if the first word is capitalized and string ends with punctuation and returns false for all other cases.
 # –––––––––––––––––––––––––––––
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]/) && text.scan(/[.|!|?]\z/)
+  if text.match(/\A[A-Z]/) && text.scan(/[.|!|?]\z/) ? true : false
     true
   else
     false
