83dcfd93ad111d3aec9e1065ef779efe17f7ca7e
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-30 20:05:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ end
 # Returns an array of words that are five letters long.
 # –––––––––––––––––––––––––––––
 def words_five_letters_long(text)
-  text.match(/\b\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 # Returns true if the first word is capitalized and string ends with punctuation and returns false for all other cases.
